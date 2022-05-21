e236b31fda1f3a3906aa328caaf41885e4b6b97a
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-05-21 15:28:55 -0700

    update prompts

--- index.js
+++ index.js
@@ -96,11 +96,186 @@ const addManager = () => {
                 addIntern();
                 break;
             default:
-                writeToFile('./dist/index.html', generateHTML(teamArr))
+                generateFile('./dist/index.html', generateHTML(teamArr))
         }
     })
 }
 
 //Engineer Prompt
+const addEngineer = () => {
+    inquirer.prompt([
+        {   
+            //name
+            type: 'input',
+            name: 'name',
+            message: 'Enter the engineer\'s name.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the engineer\'s name.');
+                    return false;
+                }
+            }
+        },
+        {   
+            //id
+            type: 'input',
+            name: 'id',
+            message: 'Enter the engineer\'s ID.',
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log('Please enter the engineer\'s ID.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {   
+            //email
+            type: 'input',
+            name: 'email',
+            message: 'Enter the engineer\'s email.',
+            validate: email => {
+                valid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+                if (valid) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email address.');
+                    return false;
+                }
+            }
+        },
+        {
+            //github
+            type: 'input',
+            name: 'github',
+            message: 'Enter the engineer\'s GitHub username.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the engineer\'s GitHub username.');
+                    return false;
+                }
+            }
+        },
+        {   
+            //add team member
+            type: 'list',
+            name: 'addTeam',
+            message: 'Would you like to add another team member?',
+            choices: ['Engineer', 'Intern', 'No']
+        }
+    ])
+    .then((answers) => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        teamArr.push(engineer);
+        switch(answers.addTeam) {
+            case 'Engineer':
+                addEngineer();
+                break;
+            case 'Intern':
+                addIntern();
+                break;
+            default:
+                generateFile('./dist/index.html', generateHTML(teamArr))
+        }
+    })
+}
 
 //Intern Prompt
+const addIntern = () => {
+    inquirer.prompt([
+        {   
+            //name
+            type: 'input',
+            name: 'name',
+            message: 'Enter the intern\'s name.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the intern\'s name.');
+                    return false;
+                }
+            }
+        },
+        {   
+            //id
+            type: 'input',
+            name: 'id',
+            message: 'Enter the intern\'s ID.',
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log('Please enter the intern\'s ID.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {   
+            //email
+            type: 'input',
+            name: 'email',
+            message: 'Enter the intern\'s email?',
+            validate: email => {
+                valid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+                if (valid) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email address.');
+                    return false;
+                }
+            }
+        },
+        {   
+            //school
+            type: 'input',
+            name: 'school',
+            message: 'Enter the intern\'s school.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the intern\'s school.');
+                    return false;
+                }
+            }
+        },
+        {   
+            //add team member
+            type: 'list',
+            name: 'addTeam',
+            message: 'Would you like to add another team member?',
+            choices: ['Engineer', 'Intern', 'No']
+        }
+    ])
+    .then((answers) => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        teamArr.push(intern);
+        switch(answers.addTeam) {
+            case 'Engineer':
+                addEngineer();
+                break;
+            case 'Intern':
+                addIntern();
+                break;
+            default:
+                generateFile('./dist/index.html', generateHTML(teamArr))
+        }
+    })
+}
+
+//Run App
+addManager();
+
+//generateFile function - generates HTML file using fs
+function generateFile(filename, data) {
+    fs.writeFile(filename, data, (err) => {
+        if(err) throw err;
+        console.log(' The team profile has been created. Please see the dist folder for the generated HTML file.');
+    });
+};
